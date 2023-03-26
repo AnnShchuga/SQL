932c5bfa673499718b73e3d1b928997a7a7ea0a1
AnnShchuga <113831909+AnnShchuga@users.noreply.github.com>
2023-03-26 13:06:34 +0300

    Update HW4.sql

--- HW4.sql
+++ HW4.sql
@@ -1,14 +1,26 @@
 -- 1. Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
-SELECT COUNT(*) as 'Likes' FROM profiles WHERE (YEAR(NOW())-YEAR(birthday)) < 12;
+SELECT 
+	COUNT(*) AS 'Общее количество лайков'
+FROM likes
+WHERE user_id IN (
+	SELECT user_id 
+	FROM profiles
+	WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 12);
+
+-- проверка количества и id пользователей, котороые удовлетворяют условиям
+SELECT * FROM profiles 
+WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 12;
 
 -- 2. Определить кто больше поставил лайков (всего): мужчины или женщины.
-SELECT 
-    CASE(gender)
-        WHEN 'm' THEN 'мужской'
-        WHEN 'f' THEN 'женский'
-        ELSE 'нет'
-    END as gender,
-    COUNT(*) as 'Количество:' FROM profiles GROUP BY gender;
+SELECT CASE (gender)
+	WHEN 'm' THEN 'Мужчины'
+	WHEN 'f' THEN 'Женщины'
+    END AS 'Больше лайков ставят:', COUNT(*) as 'Количествово лайков'
+FROM profiles p 
+JOIN likes l 
+WHERE l.user_id = p.user_id
+GROUP BY gender 
+LIMIT 1;
     
 -- 3. Вывести всех пользователей, которые не отправляли сообщения.
 SELECT DISTINCT id
